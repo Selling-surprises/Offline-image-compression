e4588ba8d6c3fc6357c511b7eedbd289ad89ac7d
Selling-surprises <163849232+Selling-surprises@users.noreply.github.com>
2026-03-02 11:19:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,18 @@ import { miaodaDevPlugin } from "miaoda-sc-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/miaoda-react-admin/', // 添加这行，仓库名替换为你实际的仓库名
-  plugins: [react(), svgr({
+  base: '/miaoda-react-admin/',
+  plugins: [
+    react(),
+    svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true,
+        exportType: 'named',
+        namedExport: 'ReactComponent',
+      },
+    }),
+    miaodaDevPlugin()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
