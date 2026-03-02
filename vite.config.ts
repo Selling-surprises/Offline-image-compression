7ed7b4b45497d04bc92cf54dcda1cbe66a9d7b30
Selling-surprises <163849232+Selling-surprises@users.noreply.github.com>
2026-03-02 10:45:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'path';
-
+import { defineConfig } from "vite";
 import { miaodaDevPlugin } from "miaoda-sc-plugin";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
+  base: '/Bookmark-validity-monitoring/',   // 👈 添加这一行
+  plugins: [
+    react(),
+    miaodaDevPlugin(),
+    svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true,
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
